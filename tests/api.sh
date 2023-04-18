9f0549625f1bd357fd140946299cda368422fe7f
4lch4 <git@4lch4.email>
2023-04-18 11:12:25 -0500

    chore: updated colors for API test script

--- tests/api.sh
+++ tests/api.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 NC='\033[0m'
-GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 BASE_URL='http://localhost:8080/api/v1'
 
-echo -e "${GREEN}Sending request to liveness endpoint...${NC}"
+echo -e "${YELLOW}Sending request to liveness endpoint...${NC}"
 
 curl -w "\n" "$BASE_URL/health/liveness"
 
-echo -e "${GREEN}Sending request to readiness endpoint...${NC}"
+echo -e "${YELLOW}Sending request to readiness endpoint...${NC}"
 
 curl -w "\n" "$BASE_URL/health/readiness"
 
-echo -e "${GREEN}Sending request to root endpoint...${NC}"
+echo -e "${YELLOW}Sending request to root endpoint...${NC}"
 
 curl -w "\n" "$BASE_URL/"
